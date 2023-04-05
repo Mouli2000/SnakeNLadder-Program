2c4ad1c4c3cedc52d3da931c905ac604d77bb7b8
Mouli2000 <moulisitthan10@gmail.com>
2023-04-05 21:32:33 +0530

    Added : Play the Game With Two Players

--- src/main/java/com/bridgelabz/snakenladder/SnakeNLadder.java
+++ src/main/java/com/bridgelabz/snakenladder/SnakeNLadder.java
@@ -3,46 +3,79 @@ package com.bridgelabz.snakenladder;
 import java.util.Random;
 
 public class SnakeNLadder {
+
 	public static void main(String[] args) {
 		
-		int player = 1;
-		int position = 0;
-		int dieRolls = 0;
-		if(player == 1 && position == 0) {
-			System.out.println("Player Number : "+player);
-			System.out.println("Starting Position : "+position);
-		}
-		else
-			System.out.println("This Game Will Be Played With Single Player...");
-		Random random = new Random();
-		for(int i=0; position<=100; i++) {
-			dieRolls++;
-			int die = random.nextInt(6)+1;
-			System.out.println("\nDie Number : "+die);
-			int options = random.nextInt(3);
-			if(position < 100) {
-				switch(options){
-				case 1: position = position + die;
-						System.out.println("Player Got Ladder !!!");
+		int player = 2;
+		int position1 = 0;
+		int position2 = 0;
+		if(player == 2 && position1 == 0 && position2 == 0) {
+			System.out.println("Players Number : "+player);
+			System.out.println("Player 1 Starting Position : "+position1);
+			System.out.println("Player 2 Starting Position : "+position2);
+			Random random = new Random();
+			while(position1<=100 || position2<=100) {
+				int die1 = random.nextInt(6)+1;
+				System.out.println("\nPlayer 1 Die Number : "+die1);
+				int options1 = random.nextInt(3);
+				if(position1 < 100) {
+					switch(options1){
+					case 1:	position1 = position1 + die1;
+							System.out.println("Player 1 Got Ladder !!!");
+							if(options1 == 1) {
+								die1 = random.nextInt(6)+1;
+								position1 = position1 + die1;
+							}
+							break;
+					case 2: position1 = position1 - die1;
+							System.out.println("Player 1 Got Snake !!!");
+							break;
+					default: System.out.println("Player 1 Has No Play..."); 
+							 position1 = position1;
+					}
+					if(position1 > 100) {position1 = position1 - die1;}
+					System.out.println("Player Position : "+position1);
+					if(position1 == 100) {
+						System.out.println("\nPlayer 1 Won The Game !!!");
 						break;
-				case 2: position = position - die;
-						System.out.println("Player Got Snake !!!");
+					}
+					else if(position1 < 0) {
+						position1 = 0;
+						System.out.println("Player 1 Position : "+position1);
+					}
+				}
+				int die2 = random.nextInt(6)+1;
+				System.out.println("\nPlayer 2 Die Number : "+die2);
+				int options2 = random.nextInt(3);
+				if(position2 < 100) {
+					switch(options2){
+					case 1: position2 = position2 + die2;
+							System.out.println("Player 2 Got Ladder !!!");
+							if(options2 == 1) {
+								die2 = random.nextInt(6)+1;
+								position2 = position2 + die2;
+							}
+							break;
+					case 2: position2 = position2 - die2;
+							System.out.println("Player 2 Got Snake !!!");
+							break;
+					default: System.out.println("Player 2 Has No Play..."); 
+							 position2 = position2;
+					}
+					if(position2 > 100) {position2 = position2 - die2;}
+					System.out.println("Player 2 Position : "+position2);
+					if(position2 == 100) {
+						System.out.println("\nPlayer 2 Won The Game !!!");
 						break;
-				default: System.out.println("No Play..."); 
-						 position = position;
-				}
-				if(position > 100) {position = position - die;}
-				System.out.println("Player Position : "+position);
-				if(position == 100) {
-					System.out.println("\nPlayer Won The Game");
-					break;
-				}
-				else if(position < 0) {
-					position = 0;
-					System.out.println("Player Psoition : "+position);
+					}
+					else if(position2 < 0) {
+						position2 = 0;
+						System.out.println("Player 2 Position : "+position2);
+					}
 				}
 			}
 		}
-		System.out.println("\nDice Rolled : "+dieRolls);
+		else
+			System.out.println("This Game Will Be Played With Two Players...");
 	}
 }
